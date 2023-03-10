6842404916067fc6c57e32d5f56e0795b269a1e5
최영후 <dudgn6392@naver.com>
2023-03-10 22:08:59 +0900

    Trigger Dead Anim

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,9 +8,22 @@ namespace RPG.Combat
     {
         [SerializeField] float health = 100f;
 
+        bool isDead;
+
         public void TakeDamage(float _damage)
         {
             health = Mathf.Max(health - _damage, 0);
+            if(health == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("Die");
         }
     }
 }
