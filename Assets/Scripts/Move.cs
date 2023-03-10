249f6c2fe581793173ceff5232c9a5de8440975d
최영후 <dudgn6392@naver.com>
2023-03-10 13:10:59 +0900

    Set NameSpace

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Move : MonoBehaviour
+namespace RPG.Movement
 {
-    // Update is called once per frame
-    void Update()
+    public class Move : MonoBehaviour
     {
-        UpdateAnimation();
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateAnimation();
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+        void UpdateAnimation()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 local = transform.InverseTransformDirection(velocity);
+            float speed = local.z;
+            GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
     }
 
-    public void MoveTo(Vector3 destination)
-    {
-        GetComponent<NavMeshAgent>().destination = destination;
-    }
-
-    void UpdateAnimation()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 local = transform.InverseTransformDirection(velocity);
-        float speed = local.z;
-        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
-    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,38 @@
+using RPG.Movement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace RPG.Control
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
+        // Start is called before the first frame update
+        void Start()
         {
-            MoveToCursor();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        void MoveToCursor()
+        {
+            Ray rayPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool isHit = Physics.Raycast(rayPoint, out hit);
+
+            if (isHit)
+            {
+                GetComponent<Move>().MoveTo(hit.point);
+            }
         }
     }
 
-    void MoveToCursor()
-    {
-        Ray rayPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool isHit = Physics.Raycast(rayPoint, out hit);
-
-        if (isHit)
-        {
-            GetComponent<Move>().MoveTo(hit.point);
-        }
-    }
 }
