cd4eff497015e1dc33f05b21d80c91e9078703f0
최영후 <dudgn6392@naver.com>
2023-03-13 18:37:18 +0900

    Player Spawn

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,19 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneIndex;
-        private void OnTriggerEnter(Collider other)
+        [SerializeField] Transform spawnPoint;
+        void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                SceneManager.LoadScene(sceneIndex);
+                StartCoroutine(TransitionCo());
             }
+        }
+
+        IEnumerator TransitionCo()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneIndex);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                return portal;
+            }
+            return null;
         }
     }
 
