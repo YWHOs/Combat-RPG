39083a36a78d9d5146662087602b02794d10a674
최영후 <dudgn6392@naver.com>
2023-03-13 20:19:11 +0900

    Save Components & GameObjects

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using RPG.Saving;
 
 namespace RPG.Movement
 {
-    public class Move : MonoBehaviour, IAction
+    public class Move : MonoBehaviour, IAction, ISaveable
     {
         [SerializeField] float maxSpeed = 6f;
 
@@ -46,6 +47,19 @@ namespace RPG.Movement
             Vector3 local = transform.InverseTransformDirection(velocity);
             float speed = local.z;
             GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 
